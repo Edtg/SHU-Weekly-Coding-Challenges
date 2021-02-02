20a82025c13fb0910fc5eebc2e794b003eb4537c
Edward Green <ed@teamgreen.co.uk>
2021-02-02 23:14:08 +0000

    Add project files.

---
+++ 01FizzBuzz/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _01FizzBuzz
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /* Modulo Operator (%)
+         * The modulo operator gets the remainder from a division operation.
+         * 
+         * e.g. 7 % 3 = 1
+         * ( 7 / 3 = 2 remainder 1)
+         */
+
+    }
+}
